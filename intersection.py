92cca2eb1425997d21e9bed46a93391bcac3b884
Garv <68726571+garvk1708@users.noreply.github.com>
2024-10-21 23:01:56 +0530

    Update intersection.py

--- intersection.py
+++ intersection.py
@@ -1,108 +1,105 @@
-
-from matplotlib_venn import  venn2 , venn2_circles ,venn3 ,venn3_circles , venn3_unweighted 
-import matplotlib_venn as venn   
+from matplotlib_venn import venn2, venn2_circles, venn3, venn3_circles  # Remove deprecated venn2_unweighted
+import matplotlib_venn as venn
 from matplotlib import pyplot as plt
-print("This algorithm  helps you to operate some of the basic set functions on venn diagrams")  
-function_set=input("type int for intersection , sum for union of two sets , diff for difference of two sets  and sym diff for difference of two sets ")
-number_set=int(input("enter 2 for operating on a pair of sets and 3 for operating on three sets"))
-setAA=set()
-setBB=set()     
-setCC=set() 
-   
-def myvenn2():
-    
-    seta_ele=int(input("enter the number of elements you want in your SET A"))
-    setb_ele=int(input("enter the number of elements you want in SET B "))   
-    
-    for i in range (1,seta_ele+1):
-        tempseta=int(input("enter element in set A")) 
-        setAA.add(tempseta)
-    for i in range(1,setb_ele+1):
-        tempsetb=int(input("enter element in set B")) 
-        setBB.add(tempsetb) 
-    venn.venn2_unweighted([setAA,setBB], set_labels = ('Set A', 'Set B')) 
-    #str_main=
-    plt.title()
-    #print(str_main)
-    plt.show()  
-    
-#myvenn2() 
 
-def myvenn3():
-    
-    seta_ele=int(input("enter the number of elements you want in your SET A"))
-    setb_ele=int(input("enter the number of elements you want in SET B "))
-    setc_ele=int(input("enter the number of elements you want in SET C "))
-    
-    for i in range (1,seta_ele+1):
-        tempseta=int(input("enter element in set A"))
-        setAA.add(tempseta)
-    for i in range(1,setb_ele+1):
-        tempsetb=int(input("enter element in set B"))
-        setBB.add(tempsetb)
-    for i in range(1,setc_ele+1):
-        tempsetc=int(input("enter element in set C"))
-        setCC.add(tempsetc)    
-     
-    venn3_unweighted ([setAA,setBB,setCC], set_labels = ('Set A', 'Set B', "Set C"))
-    
-    plt.show()  
-#myvenn3()
-#operations start from here
-def union():
-    if number_set==2:
-        str_ok=print("A ∪ B = ", setAA | setBB )
-    else:
-        str_ok=print("A ∪ B ∪ C = ", setAA | setBB | setCC)
-    plt.title("A ∪ B")       
-#union()
+while True:
+  
+    print("This algorithm helps you to operate some of the basic set functions on Venn diagrams.")
+    function_set = input("Type 'int' for intersection, 'sum' for union of two sets, 'diff' for difference of two sets, and 'sym diff' for symmetric difference of two sets: ")
+    number_set = int(input("Enter 2 for operating on a pair of sets and 3 for operating on three sets: "))
+    setAA = set()
+    setBB = set()
+    setCC = set()
 
-def inter():
-    if number_set==2:
-        str_ok=print("A ∩ B = ", setAA & setBB )
-    else:
-        str_ok=print("A ∩ B ∩ C = ", setAA & setBB & setCC) 
-    
-#inter()
-
-def diff():
-    if number_set==2:
-        str_ok=print("A - B = ", setAA - setBB )
-    else:
-        str_ok=print("A - B - C = ", setAA - setBB - setCC) 
-#diff()
-
-def symdiff():
-    if number_set==2:
-        str_ok=print("A Δ B = ", setAA ^ setBB )
-    else:
-        str_ok=print("A Δ B Δ C = ", setAA ^ setBB ^ setCC)
-#symdiff()
-
-
-
-if number_set==2:
-    myvenn2()
-    if function_set==("int"):
-        inter()
+    def myvenn2():
+        seta_ele = int(input("Enter the number of elements you want in your SET A: "))
+        setb_ele = int(input("Enter the number of elements you want in SET B: "))
         
-    elif function_set==("sum"):
-        union()
-    elif function_set==("diff"):
-        diff()
-    else:
-        symdiff()
-elif number_set==3:
-    myvenn3()
-    if function_set==("int"):
-        inter()
-    elif function_set==("sum"):
-        union()
-    elif function_set==("diff"):
-        diff()
-    else:
-        symdiff()
-else:
-    print ("invalid input error")
+        for i in range(seta_ele):
+            tempseta = int(input(f"Enter element {i+1} in set A: "))
+            setAA.add(tempseta)
+            
+        for i in range(setb_ele):
+            tempsetb = int(input(f"Enter element {i+1} in set B: "))
+            setBB.add(tempsetb)
+        
+        venn2([setAA, setBB], set_labels=('Set A', 'Set B'))  # Use venn2 instead of venn2_unweighted
+        plt.title("Venn Diagram for Set A and Set B")  # Add a meaningful title
+        plt.show()
 
+    def myvenn3():
+        seta_ele = int(input("Enter the number of elements you want in your SET A: "))
+        setb_ele = int(input("Enter the number of elements you want in SET B: "))
+        setc_ele = int(input("Enter the number of elements you want in SET C: "))
+        
+        for i in range(seta_ele):
+            tempseta = int(input(f"Enter element {i+1} in set A: "))
+            setAA.add(tempseta)
+            
+        for i in range(setb_ele):
+            tempsetb = int(input(f"Enter element {i+1} in set B: "))
+            setBB.add(tempsetb)
+            
+        for i in range(setc_ele):
+            tempsetc = int(input(f"Enter element {i+1} in set C: "))
+            setCC.add(tempsetc)
+        
+        venn3([setAA, setBB, setCC], set_labels=('Set A', 'Set B', 'Set C'))  # Use venn3 instead of venn3_unweighted
+        plt.title("Venn Diagram for Set A, Set B, and Set C")
+        plt.show()
 
+    def union():
+        if number_set == 2:
+            print("A ∪ B = ", setAA | setBB)
+            plt.title("A ∪ B")
+        else:
+            print("A ∪ B ∪ C = ", setAA | setBB | setCC)
+            plt.title("A ∪ B ∪ C")
+
+    def inter():
+        if number_set == 2:
+            print("A ∩ B = ", setAA & setBB)
+            plt.title("A ∩ B")
+        else:
+            print("A ∩ B ∩ C = ", setAA & setBB & setCC)
+            plt.title("A ∩ B ∩ C")
+
+    def diff():
+        if number_set == 2:
+            print("A - B = ", setAA - setBB)
+            plt.title("A - B")
+        else:
+            print("A - B - C = ", setAA - setBB - setCC)
+            plt.title("A - B - C")
+
+    def symdiff():
+        if number_set == 2:
+            print("A Δ B = ", setAA ^ setBB)
+            plt.title("A Δ B")
+        else:
+            print("A Δ B Δ C = ", setAA ^ setBB ^ setCC)
+            plt.title("A Δ B Δ C")
+
+    # Main logic to execute Venn diagram and set operations
+    if number_set == 2:
+        myvenn2()
+        if function_set == "int":
+            inter()
+        elif function_set == "sum":
+            union()
+        elif function_set == "diff":
+            diff()
+        else:
+            symdiff()
+    elif number_set == 3:
+        myvenn3()
+        if function_set == "int":
+            inter()
+        elif function_set == "sum":
+            union()
+        elif function_set == "diff":
+            diff()
+        else:
+            symdiff()
+    else:
+        print("Invalid input error")
